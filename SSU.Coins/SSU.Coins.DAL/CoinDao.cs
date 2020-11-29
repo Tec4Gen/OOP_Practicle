90d9faed109b163935a74a7a0dd11b22bcd01fc8
tec4gen <steam.com77@yandex.ru>
2020-11-29 17:41:50 +0400

    CRUD add

--- SSU.Coins/SSU.Coins.DAL/CoinDao.cs
+++ SSU.Coins/SSU.Coins.DAL/CoinDao.cs
@@ -6,6 +6,7 @@ using System.Configuration;
 using System.Data;
 using System.Data.SqlClient;
 
+
 namespace SSU.Coins.DAL
 {
     public class CoinDao : ICoinDao
@@ -36,17 +37,21 @@ namespace SSU.Coins.DAL
                     throw;
                 }
 
-                yield return new Coin 
+                while (reader.Read()) 
                 {
-                    Title = reader["Title"] as string,
-                    Date = (DateTime)reader["Date"],
-                    Price = reader["Price"] as int?,
-                    Anniversary = reader["Anniversary"] as string,
-                    Description = reader["Description"] as string,
-                    IdCountry = (int)reader["IdCountry"],
-                    IdMaterial = (int)reader["IdMaterial"],
-                    Picture = reader["Picture"] as byte[]
-                };
+                    yield return new Coin
+                    {
+                        Title = reader["Title"] as string,
+                        Date = (DateTime)reader["Date"],
+                        Price = reader["Price"] as int?,
+                        Anniversary = reader["Anniversary"] as string,
+                        Description = reader["Description"] as string,
+                        IdCountry = (int)reader["IdCountry"],
+                        IdMaterial = (int)reader["IdMaterial"],
+                        Picture = reader["Picture"] as byte[]
+                    };
+                }
+
 
                 yield break;
 
@@ -55,17 +60,190 @@ namespace SSU.Coins.DAL
 
         public Coin GetById(int id)
         {
-            throw new NotImplementedException();
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                var command = connection.CreateCommand();
+
+                command.CommandType = CommandType.StoredProcedure;
+                command.CommandText = "dbo.GetCoinById";
+
+                SqlParameter parameterId = new SqlParameter
+                {
+                    DbType = DbType.Int32,
+                    ParameterName = "@Id",
+                    Value = id,
+                    Direction = ParameterDirection.Input
+                };
+                command.Parameters.Add(parameterId);
+
+                try
+                {
+                    connection.Open();
+
+                    var reader = command.ExecuteReader();
+
+                    if (reader.Read())
+                    {
+                        return new Coin
+                        {
+                            Title = reader["Title"] as string,
+                            Date = (DateTime)reader["Date"],
+                            Price = reader["Price"] as int?,
+                            Anniversary = reader["Anniversary"] as string,
+                            Description = reader["Description"] as string,
+                            IdCountry = (int)reader["IdCountry"],
+                            IdMaterial = (int)reader["IdMaterial"],
+                            Picture = reader["Picture"] as byte[]
+                        };
+                    }
+                    return null;
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+            }
         }
 
         public void RemoveById(int id)
         {
-            throw new NotImplementedException();
+           using (SqlConnection connection = new SqlConnection(_connectionString))
+           {
+                var command = connection.CreateCommand();
+
+                command.CommandType = CommandType.StoredProcedure;
+                command.CommandText = "dbo.DeleteCoin";
+
+                SqlParameter parameterId = new SqlParameter 
+                {
+                    DbType = DbType.Int32,
+                    ParameterName = "@Id",
+                    Value = id,
+                    Direction = ParameterDirection.Input
+                };
+                command.Parameters.Add(parameterId);
+
+                try
+                {
+                    connection.Open();
+
+                    command.ExecuteNonQuery();
+
+                }
+                catch(Exception)
+                {
+                    throw;
+                }
+           }
         }
 
         public void Update(Coin coin)
         {
-            throw new NotImplementedException();
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                var command = connection.CreateCommand();
+
+                command.CommandType = CommandType.StoredProcedure;
+                command.CommandText = "dbo.UpdateCoin";
+
+                SqlParameter parameterId = new SqlParameter
+                {
+                    DbType = DbType.Int32,
+                    ParameterName = "@Id",
+                    Value = coin.Id,
+                    Direction = ParameterDirection.Input
+                };
+                command.Parameters.Add(parameterId);
+
+                SqlParameter parameterTitle = new SqlParameter
+                {
+                    DbType = DbType.String,
+                    ParameterName = "@Title",
+                    Value = coin.Title,
+                    Direction = ParameterDirection.Input
+                };
+                command.Parameters.Add(parameterTitle);
+
+                SqlParameter parameterDate = new SqlParameter
+                {
+                    DbType = DbType.DateTime,
+                    ParameterName = "@Date",
+                    Value = coin.Date,
+                    Direction = ParameterDirection.Input
+                };
+                command.Parameters.Add(parameterDate);
+
+
+                SqlParameter parameterPrice = new SqlParameter
+                {
+                    DbType = DbType.Int32,
+                    ParameterName = "@Price",
+                    Value = coin.Price,
+                    Direction = ParameterDirection.Input
+                };
+                command.Parameters.Add(parameterPrice);
+
+                SqlParameter parameterAnniversary = new SqlParameter
+                {
+                    DbType = DbType.String,
+                    ParameterName = "@Anniversary",
+                    Value = coin.Anniversary,
+                    Direction = ParameterDirection.Input
+                };
+                command.Parameters.Add(parameterAnniversary);
+
+                SqlParameter parameterDescription = new SqlParameter
+                {
+                    DbType = DbType.String,
+                    ParameterName = "@Description",
+                    Value = coin.Description,
+                    Direction = ParameterDirection.Input
+                };
+                command.Parameters.Add(parameterDescription);
+
+                SqlParameter parameterIdCountry = new SqlParameter
+                {
+                    DbType = DbType.Int32,
+                    ParameterName = "@IdCountry",
+                    Value = coin.IdCountry,
+                    Direction = ParameterDirection.Input
+                };
+                command.Parameters.Add(parameterIdCountry);
+
+                SqlParameter parameterIdMaterial = new SqlParameter
+                {
+                    DbType = DbType.Int32,
+                    ParameterName = "@IdMaterial",
+                    Value = coin.IdMaterial,
+                    Direction = ParameterDirection.Input
+                };
+                command.Parameters.Add(parameterIdMaterial);
+
+                SqlParameter parameterPicture = new SqlParameter
+                {
+                    DbType = DbType.Byte,
+                    ParameterName = "@Picture",
+                    Value = coin.Picture,
+                    Direction = ParameterDirection.Input
+                };
+                command.Parameters.Add(parameterPicture);
+
+                try
+                {
+                    connection.Open();
+
+                    command.ExecuteNonQuery();
+
+                }
+                catch (SqlException)
+                {
+                    throw;
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+            }
         }
     }
 }
--- SSU.Coins/SSU.Coins.DAL/CountryDao.cs
+++ SSU.Coins/SSU.Coins.DAL/CountryDao.cs
@@ -2,29 +2,166 @@
 using SSU.Coins.Entities;
 using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Data;
 
 namespace SSU.Coins.DAL
 {
     public class CountryDao : ICountryDao
     {
+        private string _connectionString = ConfigurationManager.ConnectionStrings["Country"].ConnectionString;
+
         public IEnumerable<Country> GetAll()
         {
-            throw new NotImplementedException();
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                var command = connection.CreateCommand();
+
+                command.CommandType = CommandType.StoredProcedure;
+                command.CommandText = "dbo.InsertCountry";
+
+                SqlDataReader reader;
+
+                try
+                {
+                    connection.Open();
+
+                    reader = command.ExecuteReader();
+
+                }
+                catch (Exception)
+                {
+
+                    throw;
+                }
+
+                while (reader.Read())
+                {
+                    yield return new Country { Title = reader["Title"] as string };
+                }
+                
+
+                yield break;
+
+            }
         }
 
         public Country GetById(int id)
         {
-            throw new NotImplementedException();
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                var command = connection.CreateCommand();
+
+                command.CommandType = CommandType.StoredProcedure;
+                command.CommandText = "dbo.GetCountryById";
+
+                SqlParameter parameterId = new SqlParameter
+                {
+                    DbType = DbType.Int32,
+                    ParameterName = "@Id",
+                    Value = id,
+                    Direction = ParameterDirection.Input
+                };
+                command.Parameters.Add(parameterId);
+
+                try
+                {
+                    connection.Open();
+
+                    var reader = command.ExecuteReader();
+
+                    if (reader.Read())
+                    {
+                        return new Country
+                        {
+                            Title = reader["Title"] as string,
+                        };
+                    }
+                    return null;
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+            }
         }
 
         public void RemoveById(int id)
         {
-            throw new NotImplementedException();
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                var command = connection.CreateCommand();
+
+                command.CommandType = CommandType.StoredProcedure;
+                command.CommandText = "dbo.DeleteCountry";
+
+                SqlParameter parameterId = new SqlParameter
+                {
+                    DbType = DbType.Int32,
+                    ParameterName = "@Id",
+                    Value = id,
+                    Direction = ParameterDirection.Input
+                };
+                command.Parameters.Add(parameterId);
+
+                try
+                {
+                    connection.Open();
+
+                    command.ExecuteNonQuery();
+
+                }
+                catch (Exception)
+                { 
+                    throw;
+                }
+            }
         }
 
-        public void Update(Country coin)
+        public void Update(Country country)
         {
-            throw new NotImplementedException();
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                var command = connection.CreateCommand();
+
+                command.CommandType = CommandType.StoredProcedure;
+                command.CommandText = "dbo.UpdateCountry";
+
+                SqlParameter parameterId = new SqlParameter
+                {
+                    DbType = DbType.Int32,
+                    ParameterName = "@Id",
+                    Value = country.Id,
+                    Direction = ParameterDirection.Input
+                };
+                command.Parameters.Add(parameterId);
+
+                SqlParameter parameterTitle = new SqlParameter
+                {
+                    DbType = DbType.String,
+                    ParameterName = "@Title",
+                    Value = country.Title,
+                    Direction = ParameterDirection.Input
+                };
+                command.Parameters.Add(parameterTitle);
+
+                try
+                {
+                    connection.Open();
+
+                    command.ExecuteNonQuery();
+
+                }
+                catch (SqlException)
+                {
+                    throw;
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+            }
         }
     }
 }
